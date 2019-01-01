5406e14ea927ea2c74b33dc467d205bef0bb554a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2019-01-01 20:09:54 -0200

    :memo: include an usage example of a counter

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # `@vitorluizc/animate`
 
+## Usage
+
+Call `animate`, the default exported function, with your animation function as argument.
+
+```js
+import animate from '@vitorluizc/animate';
+
+// ...
+
+const INITIAL_STATE = { count: 0 };
+
+const animation = animate(({ count } = INITIAL_STATE) => {
+  if (count === 1000)
+    return animation.stop();
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.textAlign = "center";
+  context.fillText(count, canvas.width / 2, canvas.height / 2);
+  return { count: count + 1 };
+});
+
+animation.start();
+```
+
 ## License
 
 Released under [MIT License](./LICENSE).
--- src/animate.ts
+++ src/animate.ts
@@ -6,6 +6,20 @@ export interface Animation {
 /**
  * Creates an Animation object to start and stop your animation functions. It
  * also pass state through the animation function executions.
+ * @example ```js
+ * const INITIAL_STATE = { count: 0 };
+ *
+ * const animation = animate(({ count } = INITIAL_STATE) => {
+ *   if (count === 1000)
+ *     return animation.stop();
+ *
+ *   context.clearRect(0, 0, width, height);
+ *   context.textAlign = 'center';
+ *   context.fillText(count, width / 2, height / 2);
+ *   return { count: count + 1 };
+ * });
+ *
+ * animation.start();```
  * @param animation - A function to handle animation and a state (optionally).
  */
 const animate = <S>(animation: (state: S | undefined) => S): Animation => {
